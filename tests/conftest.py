b1a5901df9e46c13d25e1b4dbad076f6b9fde6b5
krychu <samp.krystian@gmail.com>
2025-08-19 00:26:52 +0200

    Remove tests

--- tests/conftest.py
+++
@@ -1,8 +0,0 @@
-# tests/conftest.py
-import os
-import sys
-
-# Add project root to sys.path so `import hrm` works when running `pytest` from anywhere
-PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-if PROJECT_ROOT not in sys.path:
-    sys.path.insert(0, PROJECT_ROOT)
